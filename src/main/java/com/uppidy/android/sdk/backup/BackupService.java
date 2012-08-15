f11d0c780060757c19fea345662e4c0fbb3aad74
Vyacheslav Mukhortov <vm@inteks.ru>
2012-08-15 08:21:48 +0700

    MessageProvider interface simplified

--- src/main/java/com/uppidy/android/sdk/backup/BackupService.java
+++ src/main/java/com/uppidy/android/sdk/backup/BackupService.java
@@ -3,10 +3,9 @@
  */
 package com.uppidy.android.sdk.backup;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 
 import org.springframework.social.connect.ConnectionRepository;
 
@@ -16,8 +15,6 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import com.uppidy.android.sdk.api.ApiContactInfo;
-import com.uppidy.android.sdk.api.ApiMessage;
 import com.uppidy.android.sdk.api.ApiSync;
 import com.uppidy.android.sdk.api.BackupOperations;
 import com.uppidy.android.sdk.api.Uppidy;
@@ -93,7 +90,7 @@ public abstract class BackupService extends IntentService
 	 * This method is called on any exception caught during Intent processing.
 	 * @param ex
 	 */
-	protected void onError( Exception ex, Intent intent )
+	protected void onError( Exception ex, Intent intent, MessageProvider mp )
 	{
 		String msg = ex.getMessage();
 		Log.d( TAG, msg == null ? ex.toString() : msg );
@@ -134,13 +131,24 @@ public abstract class BackupService extends IntentService
 	@Override
 	protected void onHandleIntent(Intent intent)
 	{
+		MessageProvider mp = null;		
 		try
 		{
 			String action = intent.getAction();
-			MessageProvider mp;
+
 			if( action.equals( ACTION_BACKUP_ALL ) ) 
 	    	{
-				if( isEnabled() ) backupAll(); 
+				boolean somethingDone = true;
+				while( somethingDone )
+				{
+					somethingDone = false;
+					Iterator<MessageProvider> mpi = providers.values().iterator();
+					while( mpi.hasNext() )
+					{
+						mp = mpi.next();
+						somethingDone |= doBackup( mp );
+					}
+				};
 	    	}
 			// user-defined intents     
 			else if( (mp = providers.get(action)) != null ) 
@@ -159,33 +167,18 @@ public abstract class BackupService extends IntentService
 		} 
 		catch( Exception ex )
 		{
-			onError( ex, intent );
+			onError( ex, intent, mp );
 		}
-	}
-
-	private void backupAll()
-	{
-		if( !isOnline() ) return;
-		// keep synchronizing portion by portion until there is nothing to do
-		boolean somethingDone;
-		do
-		{
-			somethingDone = false;
-			for( MessageProvider mp : providers.values() )
-			{
-				somethingDone |= doBackup( mp );
-			}
-		} while( somethingDone );
 	}
 	
 	private boolean doBackup( MessageProvider mp )
 	{
 		if( mp == null || !isOnline() || !isEnabled() ) return false;
 		
-		List<ApiMessage> messages = mp.getNextSyncBundle();
-		if( messages == null || messages.size() == 0 ) return false;
+		ApiSync sync = mp.getNextSyncBundle();
+		if( sync == null || sync.getMessages() == null || sync.getMessages().size() == 0 ) return false;
 		
-		Log.i( TAG, "Backing up " + messages.size() + " messages on container identified by " + mp.getContainerId() );
+		Log.i( TAG, "Backing up " + sync.getMessages().size() + " messages on container identified by " + mp.getContainerId() );
 
 		Uppidy uppidy = getUppidyConnectionRepository().findPrimaryConnection(Uppidy.class).getApi();
 		if( uppidy == null )
@@ -193,20 +186,11 @@ public abstract class BackupService extends IntentService
 			Log.e( TAG, "Uppidy connection not found. Backup aborted." );
 			return false;
 		}
-		List<String> addresses = new ArrayList<String>();
-		for( ApiMessage m : messages ) 
-		{
-			addresses.add( m.getFrom().getAddress() );
-			for( ApiContactInfo toRef : m.getTo() ) addresses.add( toRef.getAddress() );
-		}
 		BackupOperations backupOperations = uppidy.backupOperations();
 		
-		ApiSync sync = new ApiSync();
-		sync.setContacts(mp.getContacts(addresses));
-		sync.setMessages(messages);
 		backupOperations.sync( mp.getContainerId(), sync );
 		
-		mp.backupDone( messages );
+		mp.backupDone( sync );
 		Log.i( TAG, "Backup complete" );
 		
 		return true;
--- src/main/java/com/uppidy/android/sdk/backup/MessageProvider.java
+++ src/main/java/com/uppidy/android/sdk/backup/MessageProvider.java
@@ -3,10 +3,7 @@
  */
 package com.uppidy.android.sdk.backup;
 
-import java.util.List;
-
-import com.uppidy.android.sdk.api.ApiContact;
-import com.uppidy.android.sdk.api.ApiMessage;
+import com.uppidy.android.sdk.api.ApiSync;
 
 /**
  * @author Vyacheslav Mukhortov
@@ -16,19 +13,13 @@ public interface MessageProvider
 {
 	/**
 	 * Iterator method, returns the next portion of {@code Message}s to be backed up.
-	 * Implementor may return empty list or null to indicate that there is nothing to backup.
+	 * Implementor may return {@link ApiSync} with an empty list of messages or null to indicate 
+	 * that there is nothing to backup.
 	 * It seems a good idea to return a limited number of messages, especially MMS.
 	 * @return - the list of {@code Message}s to be backed up
 	 */
-	public List<ApiMessage> getNextSyncBundle();
-	
-	/**
-	 * Returns the list of contacts by their addresses extracted from the list of {@link ApiMessage}s
-	 * @param addresses
-	 * @return list of {@code Contact}s to be backed up
-	 */
-	public List<ApiContact> getContacts( List<String> addresses );
-	
+	public ApiSync getNextSyncBundle();
+		
 	/**
 	 * Must return the container id used by Uppidy server to keep messages of the type provided by this {@link MessageProvider}.
 	 * @return String - Uppidy container ID
@@ -41,5 +32,6 @@ public interface MessageProvider
 	 * do not return those messages on subsequent {@code getNextSyncBundle()} calls. 
 	 * @param posts - messages being backed up
 	 */
-	public void backupDone( List<ApiMessage> messages );	
+	public void backupDone( ApiSync sync );	
+	
 }
